09092771f9d9f668c0b696a50f701dc9478ff8c0
megamendhie <imendhie@yahoo.com>
2023-08-10 11:09:12 +0100

    Add new const variables

--- app/src/main/java/com/megamendhie/streamon/utils/Commons.kt
+++ app/src/main/java/com/megamendhie/streamon/utils/Commons.kt
@@ -8,6 +8,8 @@ package com.megamendhie.streamon.utils
 class Commons {
     companion object{
         const val BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+        const val TRENDING_MOVIES = "trending_movies"
+        const val POPULAR_MOVIES = "popular_movies"
 
         fun popularityScore(score: Double): Int {
             return (score * 10).toInt()
